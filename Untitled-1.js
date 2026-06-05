1915dfc74aef1980932b12ee3213bd32471a1d31
Shevanava <halina.shevanava@innowise.com>
2026-06-05 08:32:34 +0300

    Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -4,11 +4,11 @@ const Name = firstName.trim().toLowerCase();
 const lastName = lasttName.trim().toLowerCase();
 const Age = "25";
 const Subscribed = "true";
-const Email = `${Name}.${lastName}@qa-test.com`;
-console.log(`=== User Profile ===
+const Email = '${Name}.${lastName}@qa-test.com';
+console.log('=== User Profile ===
 Name: ${Name}
 Last Name: ${lastName}
 Age: ${Age}
 Subscribed: ${Subscribed}
 Email: ${Email}
-====================`)
+====================')
