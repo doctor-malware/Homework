ad1a5a6bf839152e889eea1f7511bf9dadeb2edc
Shevanava <halina.shevanava@innowise.com>
2026-06-12 10:57:12 +0300

    Add files via upload

---
+++ function waitForElement(elementName, att.js
@@ -0,0 +1,12 @@
+function waitForElement(elementName, attemptsLeft) {
+    if (attemptsLeft === 0) {
+        return 'Ошибка: Элемент ${elementName} не найден.';
+    }
+   const isFound = Math.random() < 0.2; 
+   if (isFound === true) {
+    return 'Успех: Элемент ${elementName} появился!';
+   }
+   console.log('Ищем элемент ${elementName}... Осталось попыток: ${attemptsLeft}')
+   return waitForElement(elementName, attemptsLeft - 1);
+}
+console.log(waitForElement('Кнопка оплаты', 5));
