709c8965d53b7a3e9f0ba5b24704029daa3ff305
Shevanava <halina.shevanava@innowise.com>
2026-06-19 09:24:54 +0200

    Add files via upload

---
+++ Задание 5.1.js
@@ -0,0 +1,11 @@
+const baseElement = {
+  click() {
+    console.log('Клик по элементу');
+  }
+};
+const loginButton = {
+  locator: '#login',
+  text: 'Войти'
+};
+loginButton.__proto__ = baseElement;
+loginButton.click();
