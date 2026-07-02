533a42e62b69d1997eb9e643f570569a68813032
Shevanava <halina.shevanava@innowise.com>
2026-07-02 13:03:01 +0200

    Add files via upload

--- auth.spec.js
+++ auth.spec.js
@@ -6,10 +6,13 @@ test.beforeEach(async ({ page }) => {
 
 test('Successful login', async ({ page }) => {
     await page.fill('[data-test="username"]', 'standard_user');
-    await page.fill('[data-test="password"]', 'secret_sauce');
+    await page.fill('[data-test="password"]', 'secret');
 
     const loginButton = await page.locator('[data-test="login-button"]');
     await expect(loginButton).toBeVisible();
     await loginButton.click();
-    await expect(page).toHaveURL(/inventory.html/);
+
+    const ErrorMessage = await page.locator('[data-test="error"]');
+    await expect(ErrorMessage).toBeVisible();
+    await expect(ErrorMessage).toHaveText("Epic sadface: Username and password do not match any user in this service");
 });
